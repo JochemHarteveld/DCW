e9ca1da1ea3bc8314940633a89e451b53a5e2d08
Jochem Harteveld <jochemh83@gmail.com>
2022-11-16 13:25:45 +0100

    add /docs

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,22 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
+
+const dev = process.env.NODE_ENV === "development";
+
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			pages: "docs",
+			assets: "docs",
+			fallback: "200.html",
+			precompress: false
+		}),
+		trailingSlash: 'always',
+		paths: {
+			base: dev ? "" : "/DCW",
+		},
+		prerender: { entries: []}
 	}
 };
 
